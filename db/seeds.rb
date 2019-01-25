24f8d3b75c1d2be49e303ce6dd5df34ef78520c6
Tamar <tamarseleski@gmail.com>
2019-01-25 11:34:27 -0500

    Reseed and edit form page

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,16 +13,20 @@ Note.destroy_all
 
 u1 = User.create!({email: 'demouser@gmail.com', password: 'demouser'});
 n1  = Notebook.create!({title: 'notebook1', user_id: u1.id})
-Note.create!({title: 'note1', content: "content1", notebook_id: n1.id})
-Note.create!({title: 'note2', content: "content2", notebook_id: n1.id})
-Note.create!({title: 'note3', content: "content3", notebook_id: n1.id})
-Note.create!({title: 'note4', content: "content4", notebook_id: n1.id})
-Note.create!({title: 'note5', content: "content5", notebook_id: n1.id})
-Note.create!({title: 'note6', content: "content6", notebook_id: n1.id})
-Note.create!({title: 'note7', content: "content7", notebook_id: n1.id})
-Note.create!({title: 'note8', content: "content8", notebook_id: n1.id})
-Note.create!({title: 'note9', content: "content9", notebook_id: n1.id})
-Note.create!({title: 'note10', content: "content10", notebook_id: n1.id})
-Note.create!({title: 'note11', content: "content11", notebook_id: n1.id})
-Note.create!({title: 'note12', content: "content12", notebook_id: n1.id})
-Note.create!({title: 'note13', content: "content13", notebook_id: n1.id})
+Note.create!({title: 'Things to do', content: "Homework", notebook_id: n1.id})
+Note.create!({title: 'My favorite things', content: "music", notebook_id: n1.id})
+Note.create!({title: 'Recipes', content: "4 (8 ounce) packages Cream Cheese, room temperature
+1 cup granulated sugar
+1/2 cup sour cream, room temperature
+2 teaspoons vanilla
+3 eggs, room temperature", notebook_id: n1.id})
+# Note.create!({title: 'note4', content: "content4", notebook_id: n1.id})
+# Note.create!({title: 'note5', content: "content5", notebook_id: n1.id})
+# Note.create!({title: 'note6', content: "content6", notebook_id: n1.id})
+# Note.create!({title: 'note7', content: "content7", notebook_id: n1.id})
+# Note.create!({title: 'note8', content: "content8", notebook_id: n1.id})
+# Note.create!({title: 'note9', content: "content9", notebook_id: n1.id})
+# Note.create!({title: 'note10', content: "content10", notebook_id: n1.id})
+# Note.create!({title: 'note11', content: "content11", notebook_id: n1.id})
+# Note.create!({title: 'note12', content: "content12", notebook_id: n1.id})
+# Note.create!({title: 'note13', content: "content13", notebook_id: n1.id})
