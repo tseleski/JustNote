4aca8018a8383da46fea273c1727f5478b676cfc
Tamar <tamarseleski@gmail.com>
2019-01-26 20:27:39 -0500

    Save file

---
+++ frontend/components/notes/note_index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import NoteIndexItem from './note_index_item';
+import { Link } from 'react-router-dom';
+
+class NoteIndex extends React.Component {
+  componentDidMount() {
+    this.props.fetchNotes();
+  }
+
+  noteCount(){
+    if (this.props.notes.length === 1) {
+      return (
+        <p className="note-count">{this.props.notes.length} note</p>
+      )
+    } else {
+      return (
+        <p className="note-count">{this.props.notes.length} notes</p>
+      )
+    }
+     
+  }
+
+  render() {
+    const { notes } = this.props;
+    const noteList = notes.map(note => {
+      return <NoteIndexItem key={note.id} note={note} deleteNote={this.props.deleteNote} />
+    });
+    return (
+      <div>
+        <div className="fixed-header">
+          <h2>All Notes</h2>
+          {this.noteCount()}
+        </div>
+        <ul className="all-notes">{noteList}</ul>
+      </div>
+    )
+  }
+}
+
+export default NoteIndex;
