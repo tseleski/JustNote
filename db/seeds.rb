d740d75f99d9e2b90f2ec5e6884d83664f3920d3
Tamar <tamarseleski@gmail.com>
2019-01-29 16:48:09 -0500

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,18 +12,14 @@ Notebook.destroy_all
 Note.destroy_all
 
 u1 = User.create!({email: 'demouser@gmail.com', password: 'demouser'});
-n1  = Notebook.create!({title: 'notebook1', user_id: u1.id})
+n1  = Notebook.create!({title: 'General', user_id: u1.id})
 # Notebook.create!({title: 'notebook2', user_id: u1.id})
 # Notebook.create!({title: 'notebook3', user_id: u1.id})
 # Notebook.create!({title: 'notebook4', user_id: u1.id})
 # Notebook.create!({title: 'notebook5', user_id: u1.id})
-Note.create!({title: 'Things to do', content: "Homework",  plain_text: "Homework", notebook_id: n1.id})
-Note.create!({title: 'My favorite things', content: "music", plain_text: "music", notebook_id: n1.id})
-Note.create!({title: 'Recipes', content: "4 (8 ounce) packages Cream Cheese, room temperature
-1 cup granulated sugar
-1/2 cup sour cream, room temperature
-2 teaspoons vanilla
-3 eggs, room temperature", 
+Note.create!({title: 'Things to do', content: "<p><s>Homework</s></p>", plain_text: "Homework", notebook_id: n1.id})
+Note.create!({title: 'My favorite things', content: "<h1><span style='background-color: rgb(153, 51, 255); color: rgb(255, 255, 255);'>music</span></h1>", plain_text: "music", notebook_id: n1.id})
+Note.create!({title: 'Cheescake Recipe', content: "<ul><li>4 (8 ounce) packages Cream Cheese, room temperature </li><li>1 cup granulated sugar </li><li>1/2 cup sour cream, room temperature </li><li>2 teaspoons vanilla</li><li>3 eggs, room temperature</li></ul>", 
 plain_text: "4 (8 ounce) packages Cream Cheese, room temperature
 1 cup granulated sugar
 1/2 cup sour cream, room temperature
