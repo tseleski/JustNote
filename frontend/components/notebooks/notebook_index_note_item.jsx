8cd0d04708dfebb825ef329d12d9220ac15a2606
Tamar <tamarseleski@gmail.com>
2019-02-18 16:34:41 -0500

    Fix notebook index

--- frontend/components/notebooks/notebook_index_note_item.jsx
+++ frontend/components/notebooks/notebook_index_note_item.jsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 
 class NotebookIndexNoteItem extends React.Component {
   render() {
+    const title = this.props.note.title === "" ? "Untitled" : this.props.note.title;
     return (
       <div className="notebook-index-item">
         <div className="each-note">
           <div className="each-note-title">
-            <Link to={`/notebooks/${this.props.notebookId}/notes/${this.props.note.id}/edit`}><i className="fa fa-sticky-note"></i>{this.props.note.title}</Link>
+            <Link to={`/notebooks/${this.props.notebookId}/notes/${this.props.note.id}/edit`}><i className="fa fa-sticky-note"></i>{title}</Link>
           </div>
           <div className="each-note-updated"> {format(this.props.note.updated_at)}</div>
           <div className="each-note-actions"></div>
